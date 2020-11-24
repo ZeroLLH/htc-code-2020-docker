34e0972a9a05c070a5360c9b8e6bc6302beb740d
342351775@qq.com <Qq56109281>
2020-11-24 21:06:56 +0800

    Docker scripts first commit

--- trace-image/start.sh
+++ trace-image/start.sh
@@ -1,6 +1,6 @@
 if   [  $SERVER_PORT == 8002  ];
 then
-  /usr/local/src/jdk1.8.0_271/bin/java -Dserver.port=$SERVER_PORT -Xms1500m -Xmx1500m -server -jar target/trace-aggregator-0.0.1-SNAPSHOT.jar
+  /usr/local/src/jdk1.8.0_271/bin/java -Dserver.port=$SERVER_PORT -Xms1500m -Xmx1500m -server -jar /usr/local/src/trace-aggregator-0.0.1-SNAPSHOT.jar
 else
-  /usr/local/src/jdk1.8.0_271/bin/java -Dserver.port=$SERVER_PORT -Xms3500m -Xmx3500m -server -jar target/trace-filter-0.0.1-SNAPSHOT.jar --htc.filter.windowSize=100000 --htc.filter.windowQueueSize=30
+  /usr/local/src/jdk1.8.0_271/bin/java -Dserver.port=$SERVER_PORT -Xms3500m -Xmx3500m -server -jar /usr/local/src/trace-filter-0.0.1-SNAPSHOT.jar --htc.filter.windowSize=100000 --htc.filter.windowQueueSize=30
 fi
